acb5d96706c20d836880399f004a2948b8f17d94
dev-lisser <abdallah.lisser.iro@gmail.com>
2024-06-16 16:12:33 +0200

    Fichier pour la partie 1

---
+++ calcul_moyenne.php
@@ -0,0 +1,6 @@
+<?php
+$note1 = 12;
+$note2 = 15;
+$moyenne = ($note1 + $note2 + $note3) / 3;
+echo "La moyenne des trois notes est de ", $moyennes, ".\n"; 
+?>
---
+++ calcul_surface.php
@@ -0,0 +1,1 @@
+$longueur = 10; $largeur = 5; $surface == $longueur * $largeur; echo "La surface du rectangle est de ", $surfaces, " mètres carrés.\n"; 
